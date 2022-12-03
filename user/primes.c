6a45d990a7fac7f20786f3df6513ff035333ae5b
Jieqiang Wang <wangjieqiang123@163.com>
2022-12-03 23:51:56 +0800

    add primes.c version 2 concurrent version

--- user/primes.c
+++ user/primes.c
@@ -2,27 +2,35 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-pipeline(int fd_read, int num)
+void
+pipeline(int *p)
 {
-    int pd[2];
-    pipe(pd);
-    int i;
+    int pchild[2];
+    pipe(pchild);
+    int prime;
 
-    if(fork() == 0)
-    {
-        while(read(fd_read, &i, sizeof(i)))
-        {
-            if(i % num != 0)
-                write(pd[1], &i, sizeof(i));
-        }
-        close(pd[0]);
-        close(pd[1]);
+    // if process reads no more data from pipe, just exits
+    if(read(p[0], &prime, sizeof(prime)) == 0)
         exit(0);
+
+    if(fork()) // main process filters the primes
+    {
+        int n;
+        printf("prime: %d\n", prime);
+        while(read(p[0], &n, sizeof(n)))
+        {
+            if(n % prime != 0)
+                write(pchild[1], &n, sizeof(n));
+        }
+        close(p[0]);      // close parent pipe read fd
+        close(p[1]);      // close parent pipe write fd
+        close(pchild[1]); // close child pipe write fd
+        wait(0);
     }
-    wait(0);
-    close(pd[1]);
-    return pd[0];
+    else // spawn child process to read from filtered prime numbers
+    {
+        pipeline(pchild);
+    }
 }
 
 int
@@ -30,20 +38,21 @@ main(int argc, char *argv[])
 {
     int p[2];
     pipe(p);
-    int prime;
-    int fd_read = p[0];
-    
-    // the main process feeds number 2-35
-    for(int i = 2; i <= 35; i++)
-    {
-        write(p[1], &i, sizeof(int));
-    }
-    close(p[1]);
 
-    while(read(fd_read, &prime, sizeof(prime)))
+    // main process feeds number 2-35
+    if(fork())
     {
-        printf("prime %d\n", prime);
-        fd_read = pipeline(fd_read, prime);
+        for(int i = 2; i <= 35; i++)
+        {
+            write(p[1], &i, sizeof(int));
+        }
+        close(p[0]);
+        close(p[1]);
+        wait(0);
+    }
+    else // child process starts the pipeline
+    {
+        pipeline(p);
     }
 
     exit(0);
